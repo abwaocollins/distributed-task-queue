2643f26f87856a521fe25d66d29f7695252e58c0
Collins Abwao <abwaocollins060@gmail.com>
2026-03-25 09:20:47 +0300

    Update Credo checks configuration and CI linting command

--- .credo.exs
+++ .credo.exs
@@ -7,10 +7,10 @@
         excluded: [~r"/_build/", ~r"/deps/"]
       },
       checks: [
-        {Credo.Check.Design.AliasUsage},
+        {Credo.Check.Design.AliasUsage, false},
         {Credo.Check.Design.DuplicatedCode},
         {Credo.Check.Readability.MaxLineLength, [max_length: 120]},
-        {Credo.Check.Readability.ModuleDoc},
+        {Credo.Check.Readability.ModuleDoc, false},
         {Credo.Check.Readability.FunctionNames},
         {Credo.Check.Refactor.CyclomaticComplexity}
       ]
