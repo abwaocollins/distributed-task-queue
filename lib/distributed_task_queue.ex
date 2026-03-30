94a69152d5e2b40b5af5a855ad6b9f915977b212
Collins Abwao <abwaocollins060@gmail.com>
2026-03-30 16:00:20 +0300

    Implement Worker and WorkerSupervisor modules with dynamic supervision and worker registration

--- lib/distributed_task_queue.ex
+++ lib/distributed_task_queue.ex
@@ -6,4 +6,11 @@ defmodule DistributedTaskQueue do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias DistributedTaskQueue.WorkerSupervisor
+  alias DistributedTaskQueue.Worker
+
+  def start_worker(id) do
+    spec = {Worker, id}
+    DynamicSupervisor.start_child(WorkerSupervisor, spec)
+  end
 end
--- lib/distributed_task_queue/worker.ex
+++ lib/distributed_task_queue/worker.ex
@@ -1,13 +1,15 @@
 defmodule DistributedTaskQueue.Worker do
   use GenServer
 
-  def start_link(_opts) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  def start_link(id) do
+    GenServer.start_link(__MODULE__, %{id: id}, name: via(id))
+  end
+  defp via(id) do
+    {:via, Registry, {DistributedTaskQueue.WorkerRegistry, id}}
   end
 
-  def init(_) do
-    {:ok, %{}}
-
+  def init(state) do
+    {:ok, state}
   end
 
 end
