049c2f1c5263aa217708e69a6abf22d419351fe0
Collins Abwao <abwaocollins060@gmail.com>
2026-03-31 19:05:19 +0300

    Implement job management functions: add, list, update status, soft delete, and hard delete

--- lib/distributed_task_queue.ex
+++ lib/distributed_task_queue.ex
@@ -8,6 +8,93 @@ defmodule DistributedTaskQueue do
   """
   alias DistributedTaskQueue.WorkerSupervisor
   alias DistributedTaskQueue.Worker
+  alias DistributedTaskQueue.Repo
+  alias DistributedTaskQueue.{Job, Queue}
+  import Ecto.Query
+
+  #Add a job to the database
+  def add_job(queue_name, job_attrs) do
+    %Job{}
+    |> Job.changeset(Map.merge(job_attrs, %{"queue_name" => queue_name}))
+    |> Repo.insert()
+  end
+
+  #Add a queue to the database
+  def add_queue(queue_name) do
+    %Queue{}
+    |> Queue.changeset(%{"name" => queue_name})
+    |> Repo.insert()
+  end
+
+  #list all jobs
+
+  def list_jobs do
+    Repo.all(Job)
+  end
+
+  #list all queues
+  def list_queues do
+    Repo.all(Queue)
+  end
+
+  #list all pending jobs
+  def list_pending_jobs do
+    query = from j in Job, where: j.status == "pending"
+    Repo.all(query)
+  end
+
+  # list all jobs that are scheduled to run in the future
+  def list_scheduled_jobs do
+    query = from j in Job, where: j.scheduled_at > ^DateTime.utc_now()
+    Repo.all(query)
+  end
+
+  #list all jobs in a specific queue
+  def list_jobs_in_queue(queue_name) do
+    query = from j in Job, where: j.queue_name == ^queue_name
+    Repo.all(query)
+  end
+
+  # update job status
+  def update_job_status(job_id, new_status) do
+    job = Repo.get(Job, job_id)
+    if job do
+      extra = case new_status do
+        "started"   -> %{"started_at" => DateTime.utc_now()}
+        "completed" -> %{"completed_at" => DateTime.utc_now()}
+        "discarded" -> %{"discarded_at" => DateTime.utc_now()}
+        "retryable" -> %{"error_message" => "Job failed", "attempts" => job.attempts + 1, "next_retry_at" => DateTime.add(DateTime.utc_now(), 60, :second)}
+        _ -> %{}
+      end
+      job
+      |> Job.changeset(Map.merge(extra, %{"status" => new_status}))
+      |> Repo.update()
+    else
+      {:error, "Job not found"}
+    end
+  end
+
+  #soft delete a job
+  def delete_job(job_id) do
+    job = Repo.get(Job, job_id)
+    if job do
+      job
+      |> Job.changeset(%{"deleted_at" => DateTime.utc_now(), "status" => "deleted"})
+      |> Repo.update()
+    else
+      {:error, "Job not found"}
+    end
+  end
+
+  #permanently delete a job
+  def hard_delete_job(job_id) do
+    job = Repo.get(Job, job_id)
+    if job do
+      Repo.delete(job)
+    else
+      {:error, "Job not found"}
+    end
+  end
 
   def start_worker(id) do
     spec = {Worker, id}
