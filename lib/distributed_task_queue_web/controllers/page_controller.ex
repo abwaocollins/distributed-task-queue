6fe630ee9dcb4bf93f77d9f7ca7811cf07f04944
Collins Abwao <abwaocollins060@gmail.com>
2026-09-25 17:35:03 +0300

    refactor: remove unused PageController, PageHTML, and home template

--- lib/distributed_task_queue_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule DistributedTaskQueueWeb.PageController do
-  use DistributedTaskQueueWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
